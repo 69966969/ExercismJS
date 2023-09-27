bd104f8f1be1844d0d380f7ed84590923a010b82
Artis! <artislotko@gmail.com>
2023-09-27 09:59:11 +0300

    completed the null/undefined exercise and I continue to become a lasagna master

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -10,7 +10,12 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  const Visitor = {
+    name,
+    age,
+    ticketId,
+  }
+  return Visitor;
 }
 
 /**
@@ -20,7 +25,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  visitor.ticketId = null;
+  return visitor;
 }
 
 /**
@@ -31,7 +37,13 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  if (tickets[ticketId]=== null) {
+    return 'not sold';
+  } else if (tickets[ticketId]=== undefined) {
+    return 'unknown ticket id'
+  } else {
+    return `sold to ${tickets[ticketId]}`
+  }
 }
 
 /**
@@ -43,7 +55,11 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  if (tickets[ticketId] === null || tickets[ticketId] === undefined) {
+    return 'invalid ticket !!!'
+  } else {
+    return `${tickets[ticketId]}`
+  }
 }
 
 /**
@@ -53,5 +69,7 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  if (visitor.gtc?.version) {
+    return visitor.gtc.version;
+  }
 }
